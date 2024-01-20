e8ad0541d0133a618e8297b6aaaca1ec57b8e9b9
XIA <alexandre.xia@etu.sorbonne-universite.fr>
2024-01-20 16:23:36 +0100

    pull

--- cacla/caclatorch.py
+++ cacla/caclatorch.py
@@ -234,7 +234,7 @@ class Cacla(nn.Module):
     # Loss computation
     def compute_critic_loss(self, value, target):
         delta = (target - value).detach()
-        loss =  mse_loss(value, target)
+        loss =  F.mse_loss(value, target)
         n_update = 1
         if self.with_var and delta > 0:
             n_update = torch.ceil(delta/np.sqrt(self.vart)).item()
@@ -242,7 +242,7 @@ class Cacla(nn.Module):
         return loss, int(n_update)
     
     def compute_actor_loss(self, value, target):
-        return mse_loss(value, target)
+        return F.mse_loss(value, target)
     
     # Exploration
     def sample(self, x):
@@ -307,7 +307,7 @@ def train(eval_env,
         if (target_v - v0).item() > 0: 
             for _ in range(n_update):
                 optim_actor.zero_grad()
-                mse_loss(model.actor_forward(obs), action).backward()
+                F.mse_loss(model.actor_forward(obs0), action).backward()
                 optim_actor.step()
         if done or truncated: obs,_ = train_env.reset()
         if it % (100*(1<<count)) == 0:
@@ -336,8 +336,8 @@ n_runs = 20
 # Model hyperparameters
 discount_factor = 0.95
 noise_std= 0.3
-exploration = 'gaussian'
-var = False
+exploration = 'epsilon'
+var = True
 varname = 'var' if var else 'novar'
 path = f'log/{exploration}/32_{varname}_normobs_gamma{int(discount_factor*100)}_std{noise_std}'
 
